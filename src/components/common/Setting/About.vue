be85dddc8c00358dca9d79fe045d3d3a1bca9fdc
lzmmcns <67579674+lzmmcns@users.noreply.github.com>
2023-05-04 16:25:05 +0800

    Update About.vue

--- src/components/common/Setting/About.vue
+++ src/components/common/Setting/About.vue
@@ -46,18 +46,11 @@ onMounted(() => {
       </h2>
       <div class="p-2 space-y-2 rounded-md bg-neutral-100 dark:bg-neutral-700">
         <p>
-          此项目开源于
-          <a
-            class="text-blue-600 dark:text-blue-500"
-            href="https://github.com/Chanzhaoyu/chatgpt-web"
-            target="_blank"
-          >
-            GitHub
-          </a>
-          ，免费且基于 MIT 协议，没有任何形式的付费行为！
+          根据ChatGPT官方模型部署的AI
+          如在其他地方遇到与之相似网站，请自行甄别真假，如有损失，自行负责.
         </p>
         <p>
-          如果你觉得此项目对你有帮助，请在 GitHub 帮我点个 Star 或者给予一点赞助，谢谢！
+          Hashpay Wallet Foundation LTD.
         </p>
       </div>
       <p>{{ $t("setting.api") }}：{{ config?.apiModel ?? '-' }}</p>
